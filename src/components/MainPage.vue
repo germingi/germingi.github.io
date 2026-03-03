b8d52ccfba3c343e3ddeae91e6753445e96699bb
gsproston <gsproston94@gmail.com>
2026-03-03 20:23:11 +0000

    First draft

---
+++ src/components/MainPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-page">
+    <h1>germingi.com</h1>
+    <hr />
+    <!-- additional content can go here -->
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.main-page {
+  padding: 1rem;
+}
+
+h1 {
+  color: #222;
+  text-align: left;
+  margin: 0;
+}
+
+hr {
+  margin: 1rem 0;
+  border: none;
+  border-top: 2px solid #444;
+}
+</style>
