2fa6dfedd0240134f5dfda9902efcc8e1f16889d
gsproston <gsproston94@gmail.com>
2026-03-03 20:35:40 +0000

    Hello world!

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,7 +2,7 @@
   <div class="main-page">
     <h1>germingi.com</h1>
     <hr />
-    <!-- additional content can go here -->
+    hello world!
   </div>
 </template>
 
@@ -10,13 +10,14 @@
 
 <style scoped>
 .main-page {
+  --text-color: #1d1d35;
+  color: var(--text-color);
   max-width: 50rem;
   margin: 0 auto;
   padding: 1rem 2rem;
 }
 
 h1 {
-  color: #222;
   text-align: left;
   margin: 0;
 }
@@ -24,6 +25,6 @@ h1 {
 hr {
   margin: 1rem 0;
   border: none;
-  border-top: 0.125rem solid #444;
+  border-top: 0.125rem solid var(--text-color);
 }
 </style>
